bae53754d5ab9de90d94e2d1441c31464e5d9352
daivd_abu <david@livly.com>
2024-08-31 19:55:35 +0300

    remove old interfaces

--- src/interfaces/routes.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { Server as SocketIOServer } from 'socket.io';
-
-export interface Routes {
-  path?: string;
-  router: Router;
-  initializeSocketEvents?: (io: SocketIOServer) => void;
-}
